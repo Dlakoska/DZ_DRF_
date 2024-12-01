9d832c44bf08b1ad8ea507c37725412e85caf8bb
Titov Evgenii <dlakoska@mail.ru>
2024-12-01 19:58:57 +0300

    dz drf 26.2 last fix

--- courses/tasks.py
+++ courses/tasks.py
@@ -1,12 +1,7 @@
-from datetime import timedelta
-from dateutil.relativedelta import relativedelta
 from celery import shared_task
 from django.core.mail import send_mail
-from django.utils import timezone
-
 from config.settings import EMAIL_HOST_USER
 from courses.models import Course, Subscription
-from users.models import User
 
 
 @shared_task
@@ -21,15 +16,3 @@ def send_info(course_id: int) -> None:
               message=message,
               from_email=EMAIL_HOST_USER,
               recipient_list=emails_list)
-
-
-@shared_task
-def deactivate_user():
-    """Деактивирует пользователей, которые не входили в систему более 30 дней."""
-    users = User.objects.filter(last_login__isnull=False)
-    today = timezone.now()
-    month_ago = today - relativedelta(months=1)
-    users = User.objects.filter(last_login__lte=month_ago, is_active=True)
-    users.update(is_active=False)
-
-
